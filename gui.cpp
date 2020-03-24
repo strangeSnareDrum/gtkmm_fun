9b88cba63ccad76e5ddb2da8d3b6a320bf7fc4a0
Christian Hanner <christian.hanner@gmail.com>
2020-03-24 22:24:01 +0100

    Add and show label

--- gui.cpp
+++ gui.cpp
@@ -2,16 +2,18 @@
 
 #include <iostream>
 
-Gui::Gui() : mBox(Gtk::ORIENTATION_VERTICAL), mLabel("") {
+Gui::Gui() : mBox(Gtk::ORIENTATION_VERTICAL), mLabel("roof") {
     std::cout << "Gui constructor" << std::endl;
 
     // Set title and border of the window
     set_title("t h e g u i");
     set_border_width(0);
+    set_default_size(200, 200);
 
     add(mBox);
     mBox.pack_start(mLabel);
     mLabel.set_visible(true);
+    show_all();
 }
 
 void Gui::setText(std::string str) {
